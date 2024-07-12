ea4f9cdf36047f77cacfb5053014d6c4c0afd844
Triola Michelle Jackson <triolajackson@gmail.com>
2024-07-12 07:35:09 -0600

    added the audio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Description from './components/Description';
+
 import Footer from './components/Footer';
 
 export default function App() {
